3c3552bf38b2ba0b6bc70501a6d1b0b00106a925
juliflorezg <forezjuli08@hotmail.com>
2022-12-16 08:59:22 -0500

    add effect for getting movies in home

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text} from 'react-native';
+import movieDB from '../api/MovieDB';
 export const HomeScreen = () => {
+  useEffect(() => {
+    movieDB.get('/now_playing').then(res => {
+      console.log(JSON.stringify(res.data.results, null, 2));
+    });
+  });
+
   return (
     <View>
       <Text>Home Screen</Text>
