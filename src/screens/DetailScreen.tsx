5999103e3045c6b6a314ab89f39050889628f5cf
juliflorezg <forezjuli08@hotmail.com>
2022-12-17 09:41:43 -0500

    Add types to navigation stack so we know what is  passed to each Screen

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,6 +1,16 @@
+import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
 import {View, Text} from 'react-native';
-export const DetailScreen = () => {
+import {RootStackParams} from '../navigation/Navigation';
+// import {Movie} from '../interfaces/movieInterface';
+
+interface Props extends StackScreenProps<RootStackParams, 'DetailScreen'> {}
+
+export const DetailScreen = ({route}: Props) => {
+  const movie = route.params;
+
+  console.log(movie.original_title);
+
   return (
     <View>
       <Text>Detail Screen</Text>
