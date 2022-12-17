33605217ee5a5db75c6daa5a75cd678abff129af
juliflorezg <forezjuli08@hotmail.com>
2022-12-17 16:27:51 -0500

    add cast to custom hook

--- src/hooks/useMovieDetails.tsx
+++ src/hooks/useMovieDetails.tsx
@@ -1,26 +1,45 @@
 import {useState, useEffect} from 'react';
 import movieDB from '../api/movieDB';
+import {Cast, CreditsResponse} from '../interfaces/creditsInterface';
 import {FullMovie} from '../interfaces/movieInterface';
 
 interface MovieDetails {
-  cast: any[];
   isLoading: boolean;
-  fullMovie: FullMovie;
+  fullMovie?: FullMovie;
+  cast: Cast[];
 }
 
 export const useMovieDetails = (movieId: number) => {
-  const [state, setState] = useState<MovieDetails>();
+  const [state, setState] = useState<MovieDetails>({
+    isLoading: true,
+    fullMovie: undefined,
+    cast: [],
+  });
 
   const getMovieDetails = async () => {
-    const res = await movieDB.get<FullMovie>(`${movieId}`);
+    const movieDetailsPromise = await movieDB.get<FullMovie>(`${movieId}`);
+    const castPromise = await movieDB.get<CreditsResponse>(
+      `${movieId}/credits`,
+    );
 
-    console.log(res.data.overview);
+    // console.log(res.data.overview);
+
+    const [movieDetailsResponse, castPromiseResponse] = await Promise.all([
+      movieDetailsPromise,
+      castPromise,
+    ]);
+
+    setState({
+      isLoading: false,
+      fullMovie: movieDetailsResponse.data,
+      cast: castPromiseResponse.data.cast,
+    });
   };
   useEffect(() => {
     getMovieDetails();
   }, []);
 
   return {
-    state,
+    ...state,
   };
 };
