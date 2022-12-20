88f57ad709607ef67e99ce62633d96a0aef891a8
juliflorezg <forezjuli08@hotmail.com>
2022-12-20 17:39:41 -0500

    add callback to be called when fadeIn function finishes its execution

--- src/hooks/useFade.tsx
+++ src/hooks/useFade.tsx
@@ -4,18 +4,18 @@ import {Animated} from 'react-native';
 export const useFade = () => {
   const opacity = useRef(new Animated.Value(0)).current;
 
-  const fadeIn = () => {
+  const fadeIn = (callback: () => void) => {
     Animated.timing(opacity, {
       toValue: 1,
-      duration: 500,
+      duration: 300,
       useNativeDriver: true,
-    }).start();
+    }).start(() => (callback ? callback() : null));
   };
 
   const fadeOut = () => {
     Animated.timing(opacity, {
       toValue: 0,
-      duration: 500,
+      duration: 300,
       useNativeDriver: true,
     }).start();
   };
