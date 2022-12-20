1293121710764c96bacf736cd3e688c3b16dac05
juliflorezg <forezjuli08@hotmail.com>
2022-12-20 11:33:50 -0500

    use context for wrapping app in it

--- App.tsx
+++ App.tsx
@@ -2,11 +2,15 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Navigation} from './src/navigation/Navigation';
+import {GradientContextProvider} from './src/context/GradientContext';
 // import {FadeScreen} from './src/screens/FadeScreen';
+
 const App = () => {
   return (
     <NavigationContainer>
-      <Navigation />
+      <GradientContextProvider>
+        <Navigation />
+      </GradientContextProvider>
       {/* <FadeScreen /> */}
     </NavigationContainer>
   );
