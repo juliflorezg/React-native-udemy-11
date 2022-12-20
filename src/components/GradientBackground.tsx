6522eb98ef6ef5a6c3093a5d98dc4b7ea05eb4d6
juliflorezg <forezjuli08@hotmail.com>
2022-12-20 17:41:16 -0500

    add Fernandos dark magic for fading gradients (I didn't get this one lol)

--- src/components/GradientBackground.tsx
+++ src/components/GradientBackground.tsx
@@ -1,7 +1,8 @@
-import React, {useContext} from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useContext, useEffect} from 'react';
+import {View, StyleSheet, Animated} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import {GradientContext} from '../context/GradientContext';
+import {useFade} from '../hooks/useFade';
 
 interface Props {
   children: JSX.Element | JSX.Element[];
@@ -14,16 +15,33 @@ export const GradientBackground = ({
 }: // primary = '#084f6a',
 // secondary = '#75cedb',
 Props) => {
-  const {colors} = useContext(GradientContext);
+  const {previousColors, colors, setPreviousMainColors} =
+    useContext(GradientContext);
 
+  const {opacity, fadeIn, fadeOut} = useFade();
+  useEffect(() => {
+    fadeIn(() => {
+      setPreviousMainColors(colors);
+      fadeOut();
+    });
+  }, [colors]);
   return (
     <View style={{flex: 1, backgroundColor: 'wheat'}}>
       <LinearGradient
-        colors={[colors.primary, colors.secondary]}
+        colors={[previousColors.primary, previousColors.secondary]}
         start={{x: 0.1, y: 0.1}}
         end={{x: 0.5, y: 0.7}}
         style={{...StyleSheet.absoluteFillObject}}
       />
+
+      <Animated.View style={{...StyleSheet.absoluteFillObject, opacity}}>
+        <LinearGradient
+          colors={[colors.primary, colors.secondary]}
+          start={{x: 0.1, y: 0.1}}
+          end={{x: 0.5, y: 0.7}}
+          style={{...StyleSheet.absoluteFillObject}}
+        />
+      </Animated.View>
       {children}
     </View>
   );
