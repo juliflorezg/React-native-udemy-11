98d93b93fb4b1a03ff4906f3da419a9d28b38d29
juliflorezg <forezjuli08@hotmail.com>
2022-12-20 11:08:55 -0500

    pass colors to GradientBackground via props

--- src/components/GradientBackground.tsx
+++ src/components/GradientBackground.tsx
@@ -4,13 +4,19 @@ import LinearGradient from 'react-native-linear-gradient';
 
 interface Props {
   children: JSX.Element | JSX.Element[];
+  primary?: string;
+  secondary?: string;
 }
 
-export const GradientBackground = ({children}: Props) => {
+export const GradientBackground = ({
+  children,
+  primary = '#084f6a',
+  secondary = '#75cedb',
+}: Props) => {
   return (
     <View style={{flex: 1, backgroundColor: 'wheat'}}>
       <LinearGradient
-        colors={['#084f6a', '#75cedb', 'white']}
+        colors={[primary, secondary]}
         start={{x: 0.1, y: 0.1}}
         end={{x: 0.5, y: 0.7}}
         style={{...StyleSheet.absoluteFillObject}}
